d4387290648ed2e181a87bc6cc686857b62a9809
akleymenov <wn0CNFMq>
2022-10-21 16:38:42 +0300

    тест сборки для кейлока

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
+import { ReactKeycloakProvider } from '@react-keycloak/web';
 import React from 'react';
 
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import {HomePage} from "./pages/HomePage";
 import {LoginPage} from "./pages/LoginPage";
+import keycloak from "./core/Keyloak";
 
 function App() {
-  return (<BrowserRouter>
+  return (<ReactKeycloakProvider authClient={keycloak}>
+      <BrowserRouter>
         <Routes>
           <Route index element={<HomePage />} />
           <Route path="/login" element={<LoginPage />}/>
         </Routes>
       </BrowserRouter>
+      </ReactKeycloakProvider>
   );
 }
 
