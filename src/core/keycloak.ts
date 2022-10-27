b4433c702154844c5478e97c112c518dafd24da3
akleymenov <wn0CNFMq>
2022-10-27 12:13:38 +0300

    Тестирование настроек кейлока

--- src/core/keycloak.ts
+++ src/core/keycloak.ts
@@ -1,11 +1,22 @@
-import {KeycloakAdapter} from "../keycloak/keycloakAdapter";
+import Keycloak from "keycloak-js";
 
-const keycloak = new KeycloakAdapter(
-    {
-        url: 'http://localhost:8080',
-        client_secret: 'LppzpV0LLgAvQBxrgHa7cu8kIFuB3ETl',
-        client_id: 'react-auth',
-        realm: 'keyloak-react-auth'
+// const keycloak = new Keycloak({url:'http://localhost:8090/auth', clientId: 'react-auth1', realm: 'keyloak-react-auth1'});
+const keycloak = new Keycloak('/keycloak-3.json');
+
+
+export function initKeycloak(renderApp: ()=>void){
+    keycloak.init({onLoad:'login-required', enableLogging: true, }).then(function(authenticated) {
+        console.log(authenticated ? 'authenticated' : 'not authenticated');
+        authenticated && renderApp();
+        // alert(authenticated ? 'authenticated' : 'not authenticated');
+    }).catch(function(reason) {
+        console.log(reason, 'error initial');
+        alert('failed to initialize');
+    }).then(()=>{
+        console.log(keycloak.clientSecret,
+            'secret');
+        console.log(keycloak.authenticated, 'auth')
     });
+}
 
 export default keycloak;
